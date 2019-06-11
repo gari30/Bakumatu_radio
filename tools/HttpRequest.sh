77dfbfb73061d992310fec62b4a4d1f73c3149e6
yoshiki <gariyoshi0630@gmail.com>
2019-06-12 03:18:19 +0900

    API can't be used. Want to fix the shell script.

--- tools/HttpRequest.sh
+++ tools/HttpRequest.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 # とりあえずHTTP Requestを送るやつ
-curl http://ext.nicovideo.jp/api/getthumbinfo/1556190068
+# この非公式APIはニコニコから正式に利用禁止になったため使用不可。
+#curl http://ext.nicovideo.jp/api/getthumbinfo/1556190068
 
 exit
 
